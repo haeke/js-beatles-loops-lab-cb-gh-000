4c9a57550b64500063e5acdc0cbf57a963af019f
edwin rivera <waehake@gmail.com>
2018-01-17 22:10:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   var tempArr = [];
   // loop through the musicians array
   for (var i = 0; i < musicians.length; i++) {
-    tempArr[i] = musicians[i] + ' plays ' instruments[i];
+    tempArr[i] = musicians[i] + ' plays ' + instruments[i];
   }
 
   return tempArr;
